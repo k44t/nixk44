7e0aa05882d93dfbf79efff0af5e9b5ee8a14554
devvitheloper <devvitheloper@gmail.com>
2024-11-30 13:50:38 +0100

    wip

--- scripts/list-input-devices.nix
+++ scripts/list-input-devices.nix
@@ -1,2 +1,2 @@
 {pkgs, lib,  ...}@args:
-(import <lib/scripts.nix> args).mkBashScript "list-input-devices" (builtins.readFile ./list-input-devices.sh)
+pkgs.writeScriptBin "list-input-devices" (builtins.readFile ./list-input-devices.sh)
