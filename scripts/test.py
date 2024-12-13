cae5c6d419270cae4038a2739edaf25a6a140cb8
g <g@g>
2024-12-13 13:53:33 +0100

    wip

--- scripts/test.py
+++ scripts/test.py
@@ -3,29 +3,7 @@ import json
 import os
 import os.path
 
-def myexec(command):
-  return os.popen(command).read()
-exec = myexec
 
-def is_bind_mounted_as(pointed, pointing):
-  pointers = exec(f"findmnt --noheadings --output source {pointing}")
-  regexp_pattern = r'\[(.*)\]'
-  regexp_searcher = re.compile(regexp_pattern)
-  if type(pointers) != list:
-    pointers = [pointers]
-  for pointer in pointers:
-    pointer = pointer.replace("\n", "")
-    pointer_stripped = re.sub(regexp_pattern, '', pointer)
-    mount_json_string = exec(f"findmnt --noheadings --output source,target {pointer_stripped} --json")
-    mounts_json = json.loads(mount_json_string)
-    for mount in mounts_json["filesystems"]:
-      if "[" in mount["source"]:
-        continue
-      target_root = mount["target"]
-      target_path = regexp_searcher.search(pointer).groups()[0]
-      target = os.path.join(target_root, target_path)
-      if target == pointed:
-        return True
-  return False
-
-print(is_bind_mounted_as("/lib", "/var/temporarymount/"))
+import utils_python
+from utils_python.utils_python_package.src.Apoeschllogging import Deb
+Deb("test")
