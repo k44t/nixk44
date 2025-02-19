5a16b556c3b86fc5acd0b5233551a32cb697ca82
devvitheloper <devvitheloper@gmail.com>
2025-02-19 18:21:34 +0100

    new koneo

--- scripts/start-kmonad.nix
+++ scripts/start-kmonad.nix
@@ -2,22 +2,24 @@
 pkgs.writeScriptBin "start-kmonad" ''
   #!${pkgs.bash}/bin/bash
   export PATH=${pkgs.envsubst}/bin:${pkgs.kmonad}/bin:${pkgs.list-input-devices}/bin:${pkgs.gnused}/bin:${pkgs.gnugrep}bin:$PATH
-  dev_selector=$1
-  conf=$2
-  echo device selector: $dev_selector
+  dev_regex=$1
+  conf=$3
+
+
+  export OUTDEV=$2
+
+  echo device selector: $dev_regex
   echo configuration: $conf
 
   echo finding virtual device...
-  export DEV=$(list-input-devices | grep -E "$dev_selector" | sed -E "s/:.+//g")
+  export INDEV=$(list-input-devices | grep -E "$dev_regex" | sed -E "s/:.+//g")
 
-  if [ "$DEV" == "" ]; then echo virtual device not found; exit 1; fi
+  if [ "$INDEV" == "" ]; then echo virtual device not found; exit 1; fi
 
-  echo device: "\"$DEV\""
+  echo device: "\"$INDEV\""
+  echo output device: "\"$OUTDEV\""
+  export DEV=$INDEV
   CFG=$(cat $conf | envsubst)
-  # if [ "$3" == "debug" ];
-  # then
-  #  echo $CFG
-  # fi
   echo entering kmonad process...
-  kmonad <(echo "$CFG") "''${@:3}"
+  kmonad <(echo "$CFG") "''${@:4}"
 ''
